8e1cb8af7aab18494c8c5aec3c568a8af30a4624
grce47 <gracesharma47@gmail.com>
2023-02-28 19:23:10 +0530

    Tue Feb 28 07:23:10 PM IST 2023

--- Assignment4/backend/backend/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from backend.utils import getPatient
-
-@api_view(['GET'])
-def getRoutes(request):
-    return Response(getPatient())
