9b4e3cafb288cb3cd0143bcd569b7e9c634cb9f1
grce47 <gracesharma47@gmail.com>
2023-03-03 13:20:58 +0530

    Fri Mar  3 01:20:58 PM IST 2023

--- Assignment4/backend/api/serializers.py
+++ Assignment4/backend/api/serializers.py
@@ -4,6 +4,24 @@ from job.models import UserProfile
 from django.contrib.auth.models import User
 
 
+class TransactionSerializer(ModelSerializer):
+    patient_username = SerializerMethodField('eval_pat')
+    doctor_username = SerializerMethodField('eval_doc')
+
+    def eval_pat(self, foo):
+        patient = Patient.objects.filter(id=foo.patient.id)
+        return patient.first().name
+
+    def eval_doc(self, foo):
+        doctor = User.objects.filter(id=foo.doctor.id)
+        return doctor.first().username
+
+    class Meta:
+        model = Transaction
+        fields = ('id', 'patient_username', 'doctor_username',
+                  'prescription', 'created_time')
+
+
 class UserSerializer(ModelSerializer):
     designation = SerializerMethodField('eval_job')
 
