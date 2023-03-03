5047d2d450fe2f8c545745cee94e7de303a54e88
Subhajyoti Halder <subhu@Subhajyotis-MacBook-Air.local>
2023-03-03 16:19:36 +0530

    Fri  3 Mar 2023 16:19:36 IST

--- Assignment4/frontend/src/database_administrator/dash.js
+++ Assignment4/frontend/src/database_administrator/dash.js
@@ -142,7 +142,7 @@ const DatabaseAdmin = ({admin}) => {
               <span className="app-icon" />
               <p className="app-name">Database Administration's Dashboard</p>
               <div className="search-wrapper">
-                <input className="search-input" type="text" placeholder="Search a patient" onChange={search} />
+                <input className="search-input" type="text" placeholder="Search a user" onChange={search} />
                 <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="feather feather-search" viewBox="0 0 24 24">
                   <defs />
                   <circle cx={11} cy={11} r={8} />
@@ -187,7 +187,7 @@ const DatabaseAdmin = ({admin}) => {
                 <div className="projects-status">
                   <div className="item-status">
                     <span className="status-number">{userData.length}</span>
-                    <span className="status-type">Total Patients</span>
+                    <span className="status-type">Total Users</span>
                   </div>
                 </div>
                 <div className="view-actions">
